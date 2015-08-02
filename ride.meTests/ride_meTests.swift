3e9cb52710924ddc83b0dceb28e8e5b1126f3f71
Aditya Dhingra <adityadhingra07@gmail.com>
2015-08-02 12:44:59 +0530

    Initial Commit

---
+++ ride.meTests/ride_meTests.swift
@@ -0,0 +1,36 @@
+//
+//  ride_meTests.swift
+//  ride.meTests
+//
+//  Created by Aditya Dhingra on 8/2/15.
+//  Copyright (c) 2015 Aditya Dhingra. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ride_meTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
