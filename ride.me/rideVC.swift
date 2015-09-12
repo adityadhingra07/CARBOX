36a5cefff6cdd51dcabe53798d0c18e84ba197c8
Aditya Dhingra <adityadhingra07@gmail.com>
2015-09-12 13:03:23 -0400

    posting to parse

--- ride.me/rideVC.swift
+++ ride.me/rideVC.swift
@@ -12,6 +12,57 @@ import Parse
 
 class rideVC: UIViewController {
     
+    var Day = NSDate()
+    
+    // TO INPUTS
+    
+    @IBOutlet var addto: UITextField!
+    @IBOutlet var cityto: UITextField!
+    @IBOutlet var stateto: UITextField!
+    @IBOutlet var zipto: UITextField!
+    
+    // FROM INPUTS
+    
+    @IBOutlet var addfrom: UITextField!
+    @IBOutlet var cityfrom: UITextField!
+    @IBOutlet var statefrom: UITextField!
+    @IBOutlet var zipfrom: UITextField!
+    
+    // DATE INPUT
+    
+    @IBOutlet var datepick: UIDatePicker!
+    @IBAction func datepicker(sender: AnyObject) {
+        var date:NSDate = datepick.date
+        Day = NSCalendar.currentCalendar().startOfDayForDate(date)
+        println(date)
+        println(Day)
+    }
+    
+    @IBAction func done(sender: AnyObject) {
+        
+        post_ride()
+        
+    }
+    
+    func post_ride(){
+        
+        var currentUser = PFUser.currentUser()?.username
+        var driveuser = PFObject(className:"ride")
+        driveuser["username"] = currentUser
+        driveuser["date"] = Day as NSDate
+        driveuser["addto"] = addto.text!
+        driveuser["cityto"] = cityto.text!
+        driveuser["stateto"] = stateto.text!
+        driveuser["zipto"] = zipto.text!
+        driveuser["addfrom"] = addfrom.text!
+        driveuser["cityfrom"] = cityfrom.text!
+        driveuser["statefrom"] = statefrom.text!
+        driveuser["zipfrom"] = zipfrom.text!
+        
+    }
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
