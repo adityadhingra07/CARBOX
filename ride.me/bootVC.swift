fba490a75f74b9216c739762d7bbb35a010ffac9
Aditya Dhingra <adityadhingra07@gmail.com>
2015-09-12 04:56:52 -0400

    MASTER COMMITS SUICIDE

--- ride.me/bootVC.swift
+++ ride.me/bootVC.swift
@@ -8,17 +8,36 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class bootVC: UIViewController {
     
     @IBOutlet var bootView: BootView!
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
+        
+        let delay = 5 * Double(NSEC_PER_SEC)
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         bootView.addBootAnimation { (value:Bool) -> Void in
+            dispatch_after(time, dispatch_get_main_queue()) {
             self.performSegueWithIdentifier("bootsegue", sender: self)
+            }
         }
+        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Bass_Drop_04", ofType: "wav")!)
+        println(alertSound)
+        
+        // Removed deprecated use of AVAudioSessionDelegate protocol
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+        AVAudioSession.sharedInstance().setActive(true, error: nil)
+        
+        var error:NSError?
+        audioPlayer = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
     }
     
     override func didReceiveMemoryWarning() {
